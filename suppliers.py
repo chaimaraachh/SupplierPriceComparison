bb5d1c76927d8db8fdb3ed3b34c4f330d143f3c8
chaimaraachh <chaima.raach@insat.ucar.tn>
2024-12-07 17:09:01 +0100

    Add supplier pricing data for testing and evaluation

---
+++ suppliers.py
@@ -0,0 +1,10 @@
+suppliers = {
+    "Supplier A": {
+        "Dental Floss": [(1, 9.00), (20, 160.00)],
+        "Ibuprofen": [(1, 5.00), (10, 48.00)]
+    },
+    "Supplier B": {
+        "Dental Floss": [(1, 8.00), (10, 71.00)],
+        "Ibuprofen": [(1, 6.00), (5, 25.00), (100, 410.00)]
+    }
+}
